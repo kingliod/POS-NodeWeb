47cbcb4b2dd2f3c01be5063337583c6e1924089e
John Michael Vicmudo <johnmichael.vicmudo17@gmail.com>
2023-05-09 14:15:55 +0800

    Cleaning up some mess before starting...

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/html/index.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
