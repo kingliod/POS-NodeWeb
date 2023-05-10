486ef20133ecb5ef177eccb450baa137cd443092
kingliod <johnlloydc90@gmail.com>
2023-05-10 11:26:09 +0800

    Tables, buttons, delete junk codes

--- next.js
+++ next.js
@@ -2,7 +2,7 @@ const sql = require("msnodesqlv8");
 
 const connectionString =
   "server=localhost;Database=jo;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-const query = "SELECT * from [jo].[dbo].[customer_erp]";
+const query = "SELECT * from [jo].[dbo].[pullout_listings]";
 
 sql.query(connectionString, query, (err, rows) => {
   console.log(rows);
