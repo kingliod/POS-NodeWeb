9722589e592150a07562a4cc48d1c67549fb0d25
kingliod <johnlloydc90@gmail.com>
2023-05-11 14:00:16 +0800

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const sql = require("msnodesqlv8");
 
 const app = express();
 
@@ -8,8 +9,19 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
+const connectionString =
+  "server=localhost;Database=jo;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
+const query = "SELECT * from [jo].[dbo].[pullout_listings]";
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  sql.query(connectionString, query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    } else {
+      res.render("index", { rows });
+    }
+  });
 });
 
 app.listen(3000, () => {
--- next.js
+++ next.js
@@ -1,9 +1,28 @@
+const express = require("express");
 const sql = require("msnodesqlv8");
+const path = require("path");
+
+const app = express();
 
 const connectionString =
   "server=localhost;Database=jo;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
-const query = "SELECT * from [jo].[dbo].[pullout_listings]";
+const query = "SELECT * from [jo].[dbo].[joborders]";
 
-sql.query(connectionString, query, (err, rows) => {
-  console.log(rows);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/data", (req, res) => {
+  sql.query(connectionString, query, (err, rows) => {
+    console.log(rows);
+    if (err) {
+      console.error(err);
+      res.status(500).send("Error retrieving data from database");
+    } else {
+      res.json({ rows: rows });
+    }
+  });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
